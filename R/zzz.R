53757728c66c74956c47c8226f6af5ca51767e1b
juan luis legorreta torres <111556848+juanluislegorretatorres@users.noreply.github.com>
2026-01-12 12:43:28 -0600

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,1 @@
-.onAttach <- function(libname, irtbem2pl) {
-  packageStartupMessage("¡Hello! Thank you for using my library ", irtbem2pl, ".")
-  packageStartupMessage("This is the alpha version", packageVersion(irtbem2pl), ".")
-  packageStartupMessage("data: A matrix or data.frame consists of dichotomous data
-  (1 for correct and 0 for wrong response)")
-  packageStartupMessage("Usage:")
-  packageStartupMessage("irtbem2pl( data, PriorA = c(0, 0.25),PriorB = c(0, 4),
-  InitialA = 1,  InitialB = 0,  Tol = 0.01,  max.ECycle = 100L,  max.MCycle = 3L,
-  n.decimal = 5L,  n.Quadpts = 50L,  Theta.lim = c(-4, 4),  Missing = -9,
-  ParConstraint = F,   BiasSE = F,  D = 1) ")
-}
 
